5dd60619fac1c5dc5b1b5602014dd81e697238de
staticsafe <staticsafe@entropynet.net>
2012-05-07 13:01:29 -0400

    might have gotten something

--- weather.pl
+++ weather.pl
@@ -6,6 +6,7 @@ use feature ':5.10';
 use Irssi;
 use JSON;
 use vars qw($VERSION %IRSSI);
+use Weather::Underground; # install this from CPAN
 
 $VERSION = '0.1';
 %IRSSI = (
@@ -25,9 +26,23 @@ our $owner = 'staticsafe';
 our $prefix = ".";
 
 sub get_weather_data {
+    $userlocation = $_[0];
+    $weather = Weather::Underground->new(
+        place => $userlocation,
+        debug => 0,
+    )
+    || die "Error, could not get weather: $@\n";
+
+    $gettingweather = $weather->get_weather() || die "Error calling get_weather()";
+
+    $finalresult = $gettingweather{"temperature_celsius"};
+    return $finalresult;
 }
 
 sub send_msg {
+    my ($server, $target, $text) = ($_[0], $_[1], join(' ', @_[2..$#_]));
+    return unless defined $text && $text ne '';
+    Irssi::timeout_add_once(50, sub { $server->command("MSG $target $text") }, undef);
 }
 
 sub message_public {
@@ -38,8 +53,11 @@ sub message_public {
     };
 
     given ($cmd[0]) {
-        when ($prefix . 'version') { #sends version
+        when ($prefix . 'weatherver') { #sends version
             send_msg($server, $target, "This is Fear-chan's weather module, version 0.1.")
+        }
+        when ($prefix . 'w') { #get weather
+            send_msg($server, $target, get_weather_data($cmd[1]));
         }
     }
 }
